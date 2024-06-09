4d95d7584c2cd807180681022fc0c458ebb9b4a6
katyayanisinghh <katyayanisingh111@gmail.com>
2024-06-10 03:41:51 +0530

    database stored

---
+++ New_Bookspeare/Events/CreateEventsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CreateEventsViewController.swift
+//  New_Bookspeare
+//
+//  Created by Katyayani Singh on 10/06/24.
+//
+
+import UIKit
+
+class CreateEventsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
